2bb529c4954a001f756d43ff7d90d3f69fa87797
Jeremiah Aworetan <aworetanjeremiah@gmail.com>
2022-03-16 13:08:33 +0100

    created needed variables

---
+++ script.js
@@ -0,0 +1,27 @@
+let url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json"
+
+let baseTemplate
+let values
+
+let xScale
+let yScale
+
+let width = 1200
+let height = 600
+let padding = 60
+
+let canvas = d3.select('#canvas')
+               .attr('width', width)
+               .height('height', height)
+
+let generateScales = () => {
+
+}
+
+let drawCells = () => {
+
+}
+
+let drawAxes = () => {
+
+}
